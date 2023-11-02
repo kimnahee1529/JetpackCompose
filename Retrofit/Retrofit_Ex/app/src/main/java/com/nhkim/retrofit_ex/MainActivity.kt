4270a8eaf5cced4f61fe44eedcd4d32d1a9eb15f
nhkim <nhkim1529@gmail.com>
2023-11-03 00:12:22 +0900

    Feat: Api 호출

--- Retrofit/Retrofit_Ex/app/src/main/java/com/nhkim/retrofit_ex/MainActivity.kt
+++ Retrofit/Retrofit_Ex/app/src/main/java/com/nhkim/retrofit_ex/MainActivity.kt
@@ -2,10 +2,26 @@ package com.nhkim.retrofit_ex
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val api = RetrofitInstance.getInstance().create(MyApi::class.java)
+        api.getPost1().enqueue(object : Callback<Post>{
+            override fun onResponse(call: Call<Post>, response: Response<Post>) {
+                Log.d("API1", response.body().toString())
+            }
+
+            override fun onFailure(call: Call<Post>, t: Throwable) {
+                Log.d("API1", "fail")
+            }
+
+        })
     }
 }
